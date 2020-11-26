420f05a799fcf012288c1f2fbab1bac4b87b9532
Mina Farrokhnia <m.farrokhnia1205@gmail.com>
2020-11-26 12:14:28 +0100

    Update policies.tf

--- ssm-parameter-assume-role/policies.tf
+++ ssm-parameter-assume-role/policies.tf
@@ -10,15 +10,15 @@ data "aws_iam_policy_document" "crossaccount-assume-role-policy" {
 }
 data "aws_iam_policy_document" "ssm-policies" {
   statement {
-            actions = ["ssm:GetParameter",]
-            resources = ["arn:aws:ssm:::${local.current_region}:${local.current_account_id}:parameter/slack_webhook/*"]
+            actions = ["ssm:GetParameter", "ssm:GetParameters"]
+            resources = ["arn:aws:ssm:${local.current_region}:${local.current_account_id}:parameter/slack_webhook/*"]
         }
  statement {
             actions = ["ssm:DescribeParameters",]
-            resources = ["arn:aws:ssm:eu-west-1:${local.current_account_id}:*",]
+            resources = ["arn:aws:ssm:${local.current_region}:${local.current_account_id}:*",]
         }
  statement {
             actions = ["kms:Decrypt",]
-            resources = ["arn:aws:kms:eu-west-1:${local.current_account_id}:key/slack-webhook-url",]
+            resources = ["arn:aws:kms:${local.current_region}:${local.current_account_id}:key/slack-webhook-url",]
         }     
 }
