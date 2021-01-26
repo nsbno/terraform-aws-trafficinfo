44926b2049d381c335b46c06a7f77003a2330c0a
Daniel <daniel@engfeldt.net>
2021-01-26 15:49:29 +0100

    TRAFFICINFO-510 add grafana dashboard template and configuration to create grafana resources in tf.

---
+++ ecs-microservice/grafana.tf
@@ -0,0 +1,17 @@
+resource "grafana_folder" "collection" {
+  count = (var.grafana_use_existing_folder==-1 && var.grafana_create_dashboard == true) ? 1 : 0
+  title = length(var.grafana_folder_name)>0 ? var.grafana_folder_name : title("${var.name_prefix} > ${var.service_name}")
+}
+
+resource "grafana_dashboard" "dashboard_in_folder" {
+  count = var.grafana_create_dashboard == true ? 1 : 0
+  folder = var.grafana_use_existing_folder>0 ? var.grafana_use_existing_folder : grafana_folder.collection[0].id
+  config_json = templatefile(var.grafana_template_file, {
+    "name": title("${var.service_name} ${var.environment}")
+    "environment": var.environment
+    "name_prefix": var.name_prefix
+    "application": var.service_name
+    "service_name": var.service_name
+    "uuid": filemd5("${path.module}/dashboard.tpl")
+  })
+}
