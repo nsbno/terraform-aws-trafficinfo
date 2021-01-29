8e327d2b506ced71bb810ffa94d5f0b3872d1f65
Daniel <daniel@engfeldt.net>
2021-01-29 14:14:36 +0100

    TRAFFICINFO-510 just a quick fix on grafana as well

--- ecs-microservice/grafana.tf
+++ ecs-microservice/grafana.tf
@@ -2,12 +2,16 @@
 # Configure Grafana dashboard for microservice.
 #
 # Create a dashboard using the different values provided
-# as parameters to the mciroservice module to give a single
+# as parameters to the microservice module to give a single
 # glass pane to watch the different components used by the
 # service.
 ###########################################################
 
-data "aws_region" "current" {}
+data "aws_region" "current_region" {}
+
+locals {
+  current_region     = data.aws_region.current_region.name
+}
 
 resource "grafana_folder" "collection" {
   count = (var.grafana_use_existing_folder==-1 && var.grafana_create_dashboard == true) ? 1 : 0
@@ -23,7 +27,7 @@ resource "grafana_dashboard" "dashboard_in_folder" {
     "name_prefix": var.name_prefix
     "application": var.service_name
     "service_name": var.service_name
-    "region": "eu-west-1"
+    "region": local.current_region
     "uuid": filemd5(length(var.grafana_template_file)>0 ? var.grafana_template_file : "${path.module}/default-dashboard.tpl")
   })
 }
