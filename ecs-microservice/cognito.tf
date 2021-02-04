2e0e98c9206d33ea4db7b84b7b7de8c80b8fec8a
Daniel <daniel@engfeldt.net>
2021-02-04 19:41:52 +0100

    TRAFFICINFO-487 missing index key

--- ecs-microservice/cognito.tf
+++ ecs-microservice/cognito.tf
@@ -144,7 +144,7 @@ resource "time_sleep" "wait_for_credentials" {
   create_duration = "120s"
 
   triggers = {
-    secret_id = data.aws_secretsmanager_secret_version.microservice_client_credentials.id
+    secret_id = data.aws_secretsmanager_secret_version.microservice_client_credentials[0].id
     client_id = jsondecode(data.aws_secretsmanager_secret_version.microservice_client_credentials[0].secret_string)["client_id"]
     client_secret = jsondecode(data.aws_secretsmanager_secret_version.microservice_client_credentials[0].secret_string)["client_secret"]
   }
