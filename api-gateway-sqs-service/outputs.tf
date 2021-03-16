a5f43eabe3b9817037bb737c47d84a95bac97dca
Eirik Rognø <eirik.rogno@gmail.com>
2021-03-16 13:07:52 +0100

    Fix broken reference to other module

--- api-gateway-sqs-service/outputs.tf
+++ api-gateway-sqs-service/outputs.tf
@@ -1,4 +1,4 @@
 output "api_gateway_sqs_role" {
   description = "The role to use when calling sqs from the api"
-  value       = aws_iam_role.api_gateway_sqs_role
+  value       = module.api-gateway-sqs-roles.api_gateway_sqs_role
 }
