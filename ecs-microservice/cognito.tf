4283ccac14d4e4db44464ea72958d111a0c71124
Daniel Engfeldt <daniel@engfeldt.net>
2021-03-19 14:37:09 +0100

    Trafficinfo 487.conditionally.create.resource.server.and.app.client (#21)

--- ecs-microservice/cognito.tf
+++ ecs-microservice/cognito.tf
@@ -90,38 +90,53 @@ resource "aws_ssm_parameter" "cognito-url" {
 # For example traffic-control, traffic-gui and info.
 ###########################################################
 
+locals {
+  # TODO
+  # the code is surrounded with "try" as a workaround for
+  # terraform not handling the conditional objects returned
+  # with different number of attributes, this issue has
+  # been resolved in later versions of tf 0.14.x and
+  # the workaround can be removed when this module no longer
+  # needs to be backward compatible with versions of tf below 0.14.x
+  central_cognito_resource_server = try(var.create_resource_server ?{
+    resource_server = {
+      name_prefix = "${var.name_prefix}-${var.service_name}"
+      identifier = "${var.cognito_resource_server_identifier_base}/${var.service_name}"
+
+      scopes = [for key, value in var.resource_server_scopes : {
+        scope_name = value.scope_name
+        scope_description = value.scope_description
+      }]
+    }
+  } : tomap(false), {})
+
+  # TODO same as above.
+  central_cognito_user_pool_client = try(var.create_app_client ? {
+    user_pool_client = {
+      name_prefix = "${var.name_prefix}-${var.service_name}"
+      generate_secret = true
+
+      explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
+      allowed_oauth_flows = ["client_credentials"]
+      allowed_oauth_scopes = var.app_client_scopes
+      allowed_oauth_flows_user_pool_client = true
+    }
+  }  : tomap(false), {})
+
+  # build json config content for central cognito.
+  central_congito_config_content_json = jsonencode(
+    merge(local.central_cognito_resource_server, local.central_cognito_user_pool_client))
+}
+
 # upload delegated cognito config to S3 bucket.
 # this will trigger the delegated cognito terraform pipeline and and apply the config.
 resource "aws_s3_bucket_object" "delegated-cognito-config" {
-  count =  length(var.cognito_central_bucket) > 0 ? 1 : 0
+  count =  (length(var.cognito_central_bucket) > 0) && (var.create_resource_server || var.create_app_client) ? 1 : 0
   bucket = var.cognito_central_bucket
   key    = "${length(var.cognito_central_env)>0 ? var.cognito_central_env : var.environment}/${local.current_account_id}/${var.name_prefix}-${var.service_name}.json"
   acl    = "bucket-owner-full-control"
 
-  ## TODO maybe pull this out to a template to do more advanced conditional logic.
-  content = jsonencode({
-    # Configure resource server.
-    resource_server = {
-      name_prefix = "${var.name_prefix}-${var.service_name}"
-      identifier  = "${var.cognito_resource_server_identifier_base}/${var.service_name}"
-
-      scopes = [for key, value in var.resource_server_scopes : {
-        scope_name        = value.scope_name
-        scope_description = value.scope_description
-      }]
-    }
-
-    # Configure a user pool client
-    # TODO. this should be conditionally toggled by checking create_app_client.
-    user_pool_client = {
-      name_prefix     = "${var.name_prefix}-${var.service_name}"
-      generate_secret = true
-
-      allowed_oauth_flows                  = ["client_credentials"]
-      allowed_oauth_scopes                 = var.app_client_scopes
-      allowed_oauth_flows_user_pool_client = true
-    }
-  })
+  content = local.central_congito_config_content_json
   content_type = "application/json"
 }
 
@@ -202,7 +217,7 @@ resource "aws_ssm_parameter" "central_cognito_url" {
 # See the configuration of the jwt token verification in the microservice application-cloud.yml
 # for how this is configured for each microservice.
 resource "aws_ssm_parameter" "central_cognito_jwks_url" {
-  count = var.cognito_central_enable ? 1 : 0
+  count = (var.cognito_central_enable && var.create_app_client) ? 1 : 0
   name  = "/${var.name_prefix}/config/${var.service_name}/jwksUrl"
   type  = "String"
 
