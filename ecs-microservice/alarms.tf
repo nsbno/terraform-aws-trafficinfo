ccde00af2aaa74ba742851ec0a459ab9340c41ca
Daniel <daniel@engfeldt.net>
2021-09-20 22:01:00 +0200

    RP-2 syntax and fmt

--- ecs-microservice/alarms.tf
+++ ecs-microservice/alarms.tf
@@ -30,7 +30,7 @@ resource "aws_sns_topic_subscription" "degraded_alarms_to_pagerduty" {
 
 # Lambda to send notification of alarms to Slack.
 data "aws_lambda_function" "alarms_to_slack" {
-  function_name    = "${var.name_prefix}-infra_alarms_to_slack"
+  function_name = "${var.name_prefix}-infra_alarms_to_slack"
 }
 
 # Permission to lambda to invoke from SNS degraded_alarms
@@ -90,8 +90,8 @@ resource "aws_cloudwatch_metric_alarm" "service_unhealthy" {
   statistic         = "Average"
   alarm_description = "${var.name_prefix}-${var.service_name} service has unhealthy targets"
   tags              = var.tags
-  alarm_actions     = var.alarms_critical_sns_topic_arn
-  ok_actions        = var.alarms_critical_sns_topic_arn
+  alarm_actions     = aws_sns_topic.critical_alarms.arn
+  ok_actions        = aws_sns_topic.critical_alarms.arn
 }
 
 resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
@@ -109,8 +109,8 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
   statistic         = "Average"
   alarm_description = "${var.name_prefix}-${var.service_name} has crossed the CPU usage treshold"
   tags              = var.tags
-  alarm_actions     = var.alarms_degraded_sns_topic_arn
-  ok_actions        = var.alarms_degraded_sns_topic_arn
+  alarm_actions     = aws_sns_topic.degraded_alarms.arn
+  ok_actions        = aws_sns_topic.degraded_alarms.arn
 }
 
 resource "aws_cloudwatch_metric_alarm" "high_memory_utilization" {
@@ -128,8 +128,8 @@ resource "aws_cloudwatch_metric_alarm" "high_memory_utilization" {
   statistic         = "Average"
   alarm_description = "${var.name_prefix}-${var.service_name} has crossed the memory usage treshold"
   tags              = var.tags
-  alarm_actions     = var.alarms_degraded_sns_topic_arn
-  ok_actions        = var.alarms_degraded_sns_topic_arn
+  alarm_actions     = aws_sns_topic.degraded_alarms.arn
+  ok_actions        = aws_sns_topic.degraded_alarms.arn
 }
 
 resource "aws_cloudwatch_metric_alarm" "high_latency" {
@@ -146,8 +146,8 @@ resource "aws_cloudwatch_metric_alarm" "high_latency" {
   statistic          = "Average"
   alarm_description  = "${var.name_prefix}-${var.service_name} latency is above configured treshold"
   tags               = var.tags
-  alarm_actions      = var.alarms_degraded_sns_topic_arn
-  ok_actions         = var.alarms_degraded_sns_topic_arn
+  alarm_actions      = aws_sns_topic.degraded_alarms.arn
+  ok_actions         = aws_sns_topic.degraded_alarms.arn
   treat_missing_data = "notBreaching"
 }
 
@@ -165,8 +165,8 @@ resource "aws_cloudwatch_metric_alarm" "num_errors_service" {
   statistic          = "Average"
   alarm_description  = "${var.name_prefix}-${var.service_name} has crossed the 5xx error treshold"
   tags               = var.tags
-  alarm_actions      = var.alarms_degraded_sns_topic_arn
-  ok_actions         = var.alarms_degraded_sns_topic_arn
+  alarm_actions      = aws_sns_topic.degraded_alarms.arn
+  ok_actions         = aws_sns_topic.degraded_alarms.arn
   treat_missing_data = "notBreaching"
 }
 
@@ -184,7 +184,7 @@ resource "aws_cloudwatch_metric_alarm" "num_error_logs" {
   statistic          = "Sum"
   alarm_description  = "${var.name_prefix}-${var.service_name} has logged to many errors"
   tags               = var.tags
-  alarm_actions      = var.alarms_degraded_sns_topic_arn
-  ok_actions         = var.alarms_degraded_sns_topic_arn
+  alarm_actions      = aws_sns_topic.degraded_alarms.arn
+  ok_actions         = aws_sns_topic.degraded_alarms.arn
   treat_missing_data = "notBreaching"
 }
