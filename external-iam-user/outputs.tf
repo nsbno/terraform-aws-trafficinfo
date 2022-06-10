20503416842732378ead17497c190de2d52641f5
Daniel <daniel@engfeldt.net>
2022-06-10 15:27:12 +0200

    rename ext sqs machine use to just ext_machineuser

--- external-iam-user/outputs.tf
+++ external-iam-user/outputs.tf
@@ -1,9 +1,9 @@
 output "user_name" {
   description = "The name of the IAM user."
-  value       = aws_iam_user.ext_sqs_machine_user.name
+  value       = aws_iam_user.ext_machine_user.name
 }
 
 output "user_arn" {
   description = "The ARN of the IAM user."
-  value       = aws_iam_user.ext_sqs_machine_user.arn
+  value       = aws_iam_user.ext_machine_user.arn
 }
